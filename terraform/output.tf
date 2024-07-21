32ea404c8479f6d1716f4e63c4a3cd52f6f66608
essiendaniel2013 <161163258+essiendaniel2013@users.noreply.github.com>
2024-07-21 10:14:19 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,7 @@
 output "lb_hostname" {
   description = "The DNS name of the load balancer"
   value       = aws_lb.main.dns_name
-},
+}
 output "target_group_arn" {
   value = aws_lb_target_group.main.arn
 }
